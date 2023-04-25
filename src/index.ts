f758c3e8be94606c7cc75815ee77f4fa241aeb27
Friedrich Eichenroth <friedrich.eichenroth@gmail.com>
2023-04-25 23:04:32 +0200

    update internal rules

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ type AST = string
 
 type SuccessfulParseResult = {
   success: true;
+  // having substrings here is not ideal,
+  // because it means that the parse would probably run in O(n^2) time
+  // to prevent that we should only create an AST / tree with strings after a successful parse
   ast: AST;
 };
 
@@ -17,201 +20,210 @@ type ParseResult = SuccessfulParseResult | FailedParseResult;
 // - parseAll: parses the entire input and returns a ParseResult
 // - matchAll: returns true if the entire input matches the grammar, false otherwise
 
-type GrammarRule = {
-  parse: (input: string, grammar?: Grammar<string>) => ParseResult;
-  match: (input: string, grammar?: Grammar<string>) => boolean;
+type InternalSuccessfulParseResult = {
+  success: true;
+  startPos: number;
+  endPos: number;
 };
+
+type InternalFailedParseResult = {
+  success: false;
+};
+
+type InternalParseResult = InternalSuccessfulParseResult | InternalFailedParseResult;
+
+type InternalParseFunc = (input: string, startPos: number) => InternalParseResult;
+
+type ParseFunc = (input: string) => ParseResult;
+type MatchFunc = (input: string) => boolean;
+
+const toParse = (parse: InternalParseFunc): ParseFunc => (input) => {
+  const result = parse(input, 0);
+  if (!result.success) {
+    return { success: false };
+  }
+  return { success: true, ast: input.substring(result.startPos, result.endPos) };
+};
+
+const toMatch = (parse: InternalParseFunc): MatchFunc => (input) => {
+  const result = parse(input, 0);
+  return result.success;
+};
+
+type Rule = {
+  _parse: InternalParseFunc;
+  parse: ParseFunc;
+  match: MatchFunc;
+};
+
+// type GrammarRule = {
+//   parse: (input: string, grammar?: Grammar<string>) => ParseResult;
+//   match: (input: string, grammar?: Grammar<string>) => boolean;
+// };
 
 type GrammarAlias = GrammarRule;
 
 type Grammar<T extends string> = { [key in T]: GrammarRule | GrammarAlias }
 
-// string("match_me")
+// STRING //
 
-export const string = (str: string): GrammarRule => {
-  const _parse = (input: string, startPos: number): ParseResult => {
-    if (input.substring(startPos).startsWith(str)) {
-      return { success: true, ast: str };
+export const string = (str: string): Rule => {
+  const _parse: InternalParseFunc = (input, startPos) => {
+    if (input.substring(startPos, startPos + str.length) === str) {
+      return { success: true, startPos, endPos: startPos + str.length };
     }
     return { success: false };
   };
 
-  const parse = (input: string) => _parse(input, 0);
-  const match = (input: string) => _parse(input, 0).success;
-
-  return { parse, match };
+  return { _parse, parse: toParse(_parse), match: toMatch(_parse) };
 };
 
-// char('a', 'z')
+// CHARACTER CLASS //
 
-export const char = (start: string, end: string): GrammarRule => {
+export const char = (start: string, end: string): Rule => {
   if (start.length !== 1) throw new Error('start must be a single character');
   if (end.length !== 1) throw new Error('end must be a single character');
 
-  const _parse = (input: string, startPos: number): ParseResult => {
+  const _parse: InternalParseFunc = (input, startPos) => {
     const character = input.charAt(startPos);
     if (character >= start && character <= end) {
-      return { success: true, ast: character };
+      return { success: true, startPos, endPos: startPos + 1 };
     }
     return { success: false };
   };
 
-  const parse = (input: string) => _parse(input, 0);
-  const match = (input: string) => _parse(input, 0).success;
-
-  return { parse, match };
+  return { _parse, parse: toParse(_parse), match: toMatch(_parse) };
 };
 
-// any()
+// ANY //
 
-export const any = (): GrammarRule => {
-  const _parse = (input: string, startPos: number): ParseResult => {
+export const any = (): Rule => {
+  const _parse: InternalParseFunc = (input, startPos) => {
     if (input.length > startPos) {
-      return { success: true, ast: input.charAt(startPos) };
+      return { success: true, startPos, endPos: startPos + 1 };
     }
     return { success: false };
   };
 
-  const parse = (input: string) => _parse(input, 0);
-  const match = (input: string) => _parse(input, 0).success;
-
-  return { parse, match };
+  return { _parse, parse: toParse(_parse), match: toMatch(_parse) };
 };
 
-// opt(rule)
+// OPTIONAL //
 
-export const opt = (rule: GrammarRule): GrammarRule => {
-  const _parse = (input: string, startPos: number): ParseResult => {
+export const opt = (rule: Rule): Rule => {
+  const _parse: InternalParseFunc = (input, startPos) => {
     const result = rule.parse(input.substring(startPos));
-    if (result.success) return result;
-    return { success: true, ast: '' };
+    if (result.success) {
+      return { success: true, startPos, endPos: startPos + result.ast.length };
+    }
+    return { success: true, startPos, endPos: startPos };
   };
 
-  const parse = (input: string) => _parse(input, 0);
-  const match = (input: string) => _parse(input, 0).success;
-
-  return { parse, match };
+  return { _parse, parse: toParse(_parse), match: toMatch(_parse) };
 };
 
-// zeroPlus(rule)
+// ZERO-OR-MORE //
 
-export const zeroPlus = (rule: GrammarRule): GrammarRule => {
-  const _parse = (input: string, startPos: number): ParseResult => {
-    let ast = '';
+export const zeroPlus = (rule: Rule): Rule => {
+  const _parse: InternalParseFunc = (input, startPos) => {
     let pos = startPos;
     while (true) {
       const result = rule.parse(input.substring(pos));
-      if (!result.success) break;
-      ast += result.ast;
+      if (!result.success || result.ast.length === 0) break;
       pos += result.ast.length;
     }
-    return { success: true, ast };
+    return { success: true, startPos, endPos: pos };
   };
 
-  const parse = (input: string) => _parse(input, 0);
-  const match = (input: string) => _parse(input, 0).success;
-
-  return { parse, match };
+  return { _parse, parse: toParse(_parse), match: toMatch(_parse) };
 };
 
-// onePlus(rule)
+// ONE-OR-MORE //
 
-export const onePlus = (rule: GrammarRule): GrammarRule => {
-  const _parse = (input: string, startPos: number): ParseResult => {
-    const result = zeroPlus(rule).parse(input.substring(startPos));
-    if (result.success === false) return { success: false };
-    if (result.ast.length === 0) return { success: false };
-    return { success: true, ast: result.ast };
-  };
-
-  const parse = (input: string) => _parse(input, 0);
-  const match = (input: string) => _parse(input, 0).success;
-
-  return { parse, match };
-};
-
-// and(rule)
-
-export const and = (rule: GrammarRule): GrammarRule => {
-  const _parse = (input: string, startPos: number): ParseResult => {
-    const result = rule.parse(input.substring(startPos));
-    if (result.success) return { success: true, ast: '' };
+export const onePlus = (rule: Rule): Rule => {
+  // TODO: make this cleaner
+  const _parse: InternalParseFunc = (input, startPos) => {
+    let pos = startPos;
+    let success = false;
+    while (true) {
+      const result = rule.parse(input.substring(pos));
+      if (!result.success || result.ast.length === 0) break;
+      pos += result.ast.length;
+      success = true;
+    }
+    if (success) return { success: true, startPos, endPos: pos };
     return { success: false };
   };
 
-  const parse = (input: string) => _parse(input, 0);
-  const match = (input: string) => _parse(input, 0).success;
-
-  return { parse, match };
+  return { _parse, parse: toParse(_parse), match: toMatch(_parse) };
 };
 
-// not(rule)
+// AND / POSITIVE LOOKAHEAD //
 
-export const not = (rule: GrammarRule): GrammarRule => {
-  const _parse = (input: string, startPos: number): ParseResult => {
+export const and = (rule: Rule): Rule => {
+  const _parse: InternalParseFunc = (input, startPos) => {
     const result = rule.parse(input.substring(startPos));
-    if (result.success) return { success: false };
-    return { success: true, ast: '' };
+    if (result.success) return { success: true, startPos, endPos: startPos };
+    return { success: false };
   };
 
-  const parse = (input: string) => _parse(input, 0);
-  const match = (input: string) => _parse(input, 0).success;
-
-  return { parse, match };
+  return { _parse, parse: toParse(_parse), match: toMatch(_parse) };
 };
 
-// seq([rule1, rule2, ...])
+// NOT / NEGATIVE LOOKAHEAD //
 
-export const seq = (rules: GrammarRule[]): GrammarRule => {
-  const _parse = (input: string, startPos: number): ParseResult => {
-    let ast = '';
+export const not = (rule: Rule): Rule => {
+  const _parse: InternalParseFunc = (input, startPos) => {
+    const result = rule.parse(input.substring(startPos));
+    if (result.success) return { success: false };
+    return { success: true, startPos, endPos: startPos };
+  };
+
+  return { _parse, parse: toParse(_parse), match: toMatch(_parse) };
+};
+
+// SEQUENCE //
+
+export const seq = (rules: Rule[]): Rule => {
+  const _parse: InternalParseFunc = (input, startPos) => {
     let pos = startPos;
 
     const success = rules.every((rule) => {
       const result = rule.parse(input.substring(pos));
-
       if (result.success === false) return false;
-
-      ast += result.ast;
       pos += result.ast.length;
       return true;
     });
 
-    if (!success) return { success: false };
-    return { success: true, ast };
+    if (success) return { success: true, startPos, endPos: pos };
+    return { success: false };
   };
 
-  const parse = (input: string) => _parse(input, 0);
-  const match = (input: string) => _parse(input, 0).success;
-
-  return { parse, match };
+  return { _parse, parse: toParse(_parse), match: toMatch(_parse) };
 };
 
-// choice([rule1, rule2, ...])
+// CHOICE //
 
-export const choice = (rules: GrammarRule[]): GrammarRule => {
-  const _parse = (input: string, startPos: number): ParseResult => {
-    let ast = '';
+export const choice = (rules: Rule[]): Rule => {
+  const _parse: InternalParseFunc = (input, startPos) => {
     let pos = startPos;
 
     const success = rules.some((rule) => {
       const result = rule.parse(input.substring(pos));
-
       if (result.success === false) return false;
-
-      ast += result.ast;
       pos += result.ast.length;
       return true;
     });
 
-    if (!success) return { success: false };
-    return { success: true, ast };
+    if (success) {
+      return {
+        success: true, startPos, endPos: pos,
+      };
+    }
+    return { success: false };
   };
 
-  const parse = (input: string) => _parse(input, 0);
-  const match = (input: string) => _parse(input, 0).success;
-
-  return { parse, match };
+  return { _parse, parse: toParse(_parse), match: toMatch(_parse) };
 };
 
 // alias('AliasName')
