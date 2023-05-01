be4a39983df7c5115c5db34dae6181d678ca2b97
Friedrich Eichenroth <friedrich.eichenroth@gmail.com>
2023-05-01 12:06:40 +0200

    update non-context-free test

--- test/non-context-free.ts
+++ test/non-context-free.ts
@@ -27,41 +27,17 @@ const grammar = p.grammar({
   ]),
 });
 
-// <Base> := !. / &(<A>!"b")"a"*<B>!.
-// <A> := "a" <A> "b" / "ab"
-// <B> := "b" <B> "c" / "bc"
-
-// const grammar = p.grammar({
-//   Base: p.choice([
-//       p.negLookahead(p.any()),
-//       p.sequence([
-//         p.posLookahead(p.sequence([p.alias('A'), p.negLookahead(p.string('b'))])),
-//         p.zeroOrMore(p.string('a')),
-//         p.alias('B'),
-//         p.negLookahead(p.any())
-//       ])
-//     ]),
-//   A: p.choice([
-//     p.sequence([p.string('a'), p.alias('A'), p.string('b')]),
-//     p.string('ab')
-//   ]),
-//   B: p.choice([
-//     p.sequence([p.string('b'), p.alias('B'), p.string('c')]),
-//     p.string('bc')
-//   ])
-// })
-
 test('non-context-free', (t) => {
-  t.equal(grammar.S.match(''), false);
+  t.equal(grammar.rules.S.match(''), false);
 
-  t.equal(grammar.S.match('abc'), true);
-  t.equal(grammar.S.match('aaaabbbbcccc'), true);
+  t.equal(grammar.rules.S.match('abc'), true);
+  t.equal(grammar.rules.S.match('aaaabbbbcccc'), true);
 
-  t.equal(grammar.S.match('ab'), false);
-  t.equal(grammar.S.match('abab'), false);
-  t.equal(grammar.S.match('abcd'), false);
-  t.equal(grammar.S.match('aabbc'), false);
-  t.equal(grammar.S.match('aabbccc'), false);
+  t.equal(grammar.rules.S.match('ab'), false);
+  t.equal(grammar.rules.S.match('abab'), false);
+  t.equal(grammar.rules.S.match('abcd'), false);
+  t.equal(grammar.rules.S.match('aabbc'), false);
+  t.equal(grammar.rules.S.match('aabbccc'), false);
 
   t.end();
 });
